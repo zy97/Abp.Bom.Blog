a43b1db58120bd38fe84ae2ed555652ca0ed998a
bomky <bom_ky@outlook.com>
2022-06-23 11:18:39 +0800

    添加一个简单的任务

---
+++ src/Bom.Blog.Application/JobService.cs
@@ -0,0 +1,22 @@
+﻿using Bom.Blog.Jobs;
+using System;
+using System.Threading.Tasks;
+using Volo.Abp.BackgroundJobs;
+
+namespace Bom.Blog
+{
+    public class JobService : BlogAppService
+    {
+        private readonly IBackgroundJobManager backgroundJobManager;
+
+        public JobService(IBackgroundJobManager backgroundJobManager)
+        {
+            this.backgroundJobManager = backgroundJobManager;
+        }
+
+        public async Task StartAsync()
+        {
+            await backgroundJobManager.EnqueueAsync(new TestJobArgs() { Name = $"Job ${DateTime.Now}" });
+        }
+    }
+}
---
+++ src/Bom.Blog.BackgroundJobs/Jobs/TestJob.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using Volo.Abp.BackgroundJobs;
+using Volo.Abp.DependencyInjection;
+
+namespace Bom.Blog.Jobs
+{
+    public class TestJob : AsyncBackgroundJob<TestJobArgs>, ITransientDependency
+    {
+        private readonly ILogger<TestJob> logger;
+
+        public TestJob(ILogger<TestJob> logger)
+        {
+            this.logger = logger;
+        }
+        public override Task ExecuteAsync(TestJobArgs args)
+        {
+            this.logger.LogInformation("TestJob is executed with argument: {Argument}", args.Name);
+            return Task.CompletedTask;
+        }
+    }
+    public class TestJobArgs
+    {
+        public string Name { get; set; }
+    }
+}
