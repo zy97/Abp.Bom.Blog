a32ff1e01db143219552bc06f3a8ca1f4b8be2fd
bomky <bom_ky@outlook.com>
2022-06-27 10:59:46 +0800

    实现暗主题的切换

---
+++ src/Bom.Blog.Blazor.Web/wwwroot/js/app.js
@@ -0,0 +1,19 @@
+﻿var func = window.func || {};
+func = {
+    setStorage: function (name, value) {
+        localStorage.setItem(name, value);
+    },
+    getStorage: function (name) {
+        return localStorage.getItem(name);
+    },
+    switchTheme: function () {
+        var currentTheme = this.getStorage("theme") || "Light";
+        var isDark = currentTheme === "Dark";
+        if (isDark) {
+            document.body.classList.add("dark-theme");
+        }
+        else {
+            document.body.classList.remove("dark-theme");
+        }
+    }
+}
