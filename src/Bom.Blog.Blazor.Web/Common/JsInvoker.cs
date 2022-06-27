7eb7d6dffd11756d44141b2874b914c9e08bd33e
bomky <bom_ky@outlook.com>
2022-06-27 11:14:55 +0800

    提取js操作

---
+++ src/Bom.Blog.Blazor.Web/Common/JsInvoker.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.JSInterop;
+
+namespace Bom.Blog.Blazor.Web.Common
+{
+    public class JsInvoker
+    {
+        private readonly IJSRuntime jSRuntime;
+
+        public JsInvoker(IJSRuntime jSRuntime)
+        {
+            this.jSRuntime = jSRuntime;
+        }
+        public async Task InvokeAsync(string identifier, params object[] args)
+        {
+            await jSRuntime.InvokeVoidAsync(identifier, args);
+        }
+        public async Task<TValue> InvokeAsync<TValue>(string identifier, params object[] args)
+        {
+            return await jSRuntime.InvokeAsync<TValue>(identifier, args);
+        }
+        public async Task SetStorageAsync(string key, string value)
+        {
+            await InvokeAsync("window.func.setStorage", key, value);
+        }
+        public async Task<string> GetStorageAsync(string key)
+        {
+            return await InvokeAsync<string>("window.func.getStorage", key);
+        }
+    }
+}
