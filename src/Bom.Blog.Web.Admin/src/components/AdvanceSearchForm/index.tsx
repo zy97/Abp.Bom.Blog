f2873b45795e13ba6ea3453315957f988b1a98ae
bomky <bom_ky@outlook.com>
2022-07-14 13:59:08 +0800

    添加eslint与prettier的代码检查与格式化

--- src/Bom.Blog.Web.Admin/src/components/AdvanceSearchForm/index.tsx
+++ src/Bom.Blog.Web.Admin/src/components/AdvanceSearchForm/index.tsx
@@ -1,82 +1,66 @@
-import { DownOutlined, UpOutlined } from '@ant-design/icons';
-import { Button, Col, Form, Input, Row, Select } from 'antd';
-import { FormProvider } from 'rc-field-form';
-import React, { useState } from 'react';
+import { DownOutlined, UpOutlined } from "@ant-design/icons";
+import { Button, Col, Form, Row } from "antd";
+import { FormInstance } from "antd/es/form/Form";
+import { useState } from "react";
 
-const { Option } = Select;
+type AdvanceSearchFormProps = {
+  form: FormInstance;
+  submit: () => void;
+  reset: () => void;
+  extraActions: Array<extraActionProps>;
+  children: React.ReactNode[];
+};
+type extraActionProps = {
+  content: string;
+  action: () => void;
+};
+const AdvancedSearchForm = (props: AdvanceSearchFormProps) => {
+  const { form, submit, reset, extraActions } = props;
+  const formCount = props.children.length;
+  const [expand, setExpand] = useState(false);
 
-const AdvancedSearchForm = (props: any) => {
-    const { form, submit, reset, extraActions } = props;
-    const formCount = props.children.length;
-    const [expand, setExpand] = useState(false);
+  const getFields = () => {
+    const count = formCount <= 6 ? formCount : expand ? formCount : 6;
+    const children = [];
+    for (let i = 0; i < count; i++) {
+      children.push(
+        <Col span={8} key={i}>
+          {props.children[i]}
+        </Col>
+      );
+    }
+    return children;
+  };
 
-    const getFields = () => {
-        const count = formCount <= 6 ? formCount : expand ? formCount : 6;
-        const children = [];
-        for (let i = 0; i < count; i++) {
-            children.push(
-                <Col span={8} key={i}>
-                    {/* <Form.Item
-                        name={`field-${i}`}
-                        label={`Field ${i}`}
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Input something!',
-                            },
-                        ]}
-                    >
-                        {i % 3 !== 1 ? (
-                            <Input placeholder="placeholder" />
-                        ) : (
-                            <Select defaultValue="2">
-                                <Option value="1">1</Option>
-                                <Option value="2">
-                                    longlonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglong
-                                </Option>
-                            </Select>
-                        )}
-                    </Form.Item> */}
-                    {props.children[i]}
-                </Col>
+  return (
+    <Form form={form} name="advanced_search">
+      <Row gutter={24}>{getFields()}</Row>
+      <Row>
+        <Col span={24} className="space-x-4 text-right">
+          {extraActions.map((i: extraActionProps, key: number) => {
+            return (
+              <Button type="primary" key={key} onClick={i.action}>
+                {i.content}
+              </Button>
             );
-        }
-        return children;
-    };
-
-    const onFinish = (values: any) => {
-        console.log('Received values of form: ', values);
-    };
-
-    return (
-        <Form form={form} name="advanced_search" onFinish={onFinish}>
-            <Row gutter={24}>{getFields()}</Row>
-            <Row>
-                <Col span={24} className="space-x-4 text-right">
-                    {extraActions.map((i: any, key: any) => {
-                        return (
-                            <Button type="primary" key={key} onClick={i.action}>
-                                {i.content}
-                            </Button>
-                        );
-                    })}
-                    <Button type="primary" htmlType="submit" onClick={submit}>
-                        搜索
-                    </Button>
-                    <Button onClick={reset}>重置</Button>
-                    {formCount >= 6 && (
-                        <a
-                            style={{ fontSize: 12 }}
-                            onClick={() => {
-                                setExpand(!expand);
-                            }}
-                        >
-                            {expand ? <UpOutlined /> : <DownOutlined />} 折叠
-                        </a>
-                    )}
-                </Col>
-            </Row>
-        </Form>
-    );
+          })}
+          <Button type="primary" htmlType="submit" onClick={submit}>
+            搜索
+          </Button>
+          <Button onClick={reset}>重置</Button>
+          {formCount >= 6 && (
+            <a
+              style={{ fontSize: 12 }}
+              onClick={() => {
+                setExpand(!expand);
+              }}
+            >
+              {expand ? <UpOutlined /> : <DownOutlined />} 折叠
+            </a>
+          )}
+        </Col>
+      </Row>
+    </Form>
+  );
 };
 export default AdvancedSearchForm;
--- src/Bom.Blog.Web.Admin/src/pages/BlogManage/Pages/Tag/Index.tsx
+++ src/Bom.Blog.Web.Admin/src/pages/BlogManage/Pages/Tag/Index.tsx
@@ -1,193 +1,190 @@
-import { useAntdTable, useRequest } from 'ahooks';
-import { Button, Form, Input, message, Modal, Table } from 'antd';
-import { useState } from 'react';
-import AdvancedSearchForm from '../../../../components/AdvanceSearchForm';
-import { TagDto } from '../../../../data/models/Tag';
-import useStores from '../../../../hooks/useStore';
-import Category from '../Category';
+import { useAntdTable, useRequest } from "ahooks";
+import { Button, Form, Input, message, Modal, Table } from "antd";
+import { useState } from "react";
+import AdvancedSearchForm from "../../../../components/AdvanceSearchForm";
+import { TagDto } from "../../../../data/models/Tag";
+import useStores from "../../../../hooks/useStore";
 function Tags() {
-    const { tagStore } = useStores();
-    const [visible, setVisible] = useState(false);
-    const [form] = Form.useForm();
-    const [modalForm] = Form.useForm();
-    const { tableProps, search } = useAntdTable(tagStore.getTags, {
-        defaultPageSize: 10,
-        form,
-        debounceWait: 500,
+  const { tagStore } = useStores();
+  const [visible, setVisible] = useState(false);
+  const [form] = Form.useForm();
+  const [modalForm] = Form.useForm();
+  const { tableProps, search } = useAntdTable(tagStore.getTags, {
+    defaultPageSize: 10,
+    form,
+    debounceWait: 500,
+  });
+  const { runAsync } = useRequest(tagStore.getTagById, {
+    manual: true,
+  });
+  const deleteTag = (record: TagDto) => {
+    Modal.confirm({
+      title: "删除标签",
+      content: "确定删除吗？",
+      onOk: async () => {
+        const success = await tagStore.deleteTag(record.id);
+        if (success) {
+          message.success("删除成功");
+          search.submit();
+        } else {
+          message.error("删除失败");
+        }
+      },
+      okText: "确定",
+      cancelText: "取消",
     });
-    const { runAsync } = useRequest(tagStore.getTagById, {
-        manual: true,
-    });
-    const deleteTag = (record: TagDto) => {
-        Modal.confirm({
-            title: '删除标签',
-            content: '确定删除吗？',
-            onOk: async () => {
-                const success = await tagStore.deleteTag(record.id);
-                if (success) {
-                    message.success('删除成功');
-                    search.submit();
-                } else {
-                    message.error('删除失败');
-                }
-            },
-            okText: '确定',
-            cancelText: '取消',
-        });
-    };
-    const showModal = () => {
+  };
+  const showModal = () => {
+    setVisible(true);
+  };
+  const getTag = async (record: TagDto) => {
+    try {
+      const tag = await runAsync(record.id);
+      if (tag) {
+        modalForm.setFieldsValue(tag);
+        console.log(tag);
         setVisible(true);
-    };
-    const getTag = async (record: TagDto) => {
-        try {
-            const tag = await runAsync(record.id);
-            if (tag) {
-                modalForm.setFieldsValue(tag);
-                console.log(tag);
-                setVisible(true);
-            }
-        } catch (error) {}
-    };
-    const addOrUpdateTag = async (data: TagDto) => {
-        try {
-            if (data.id) {
-                const tag = await tagStore.updateTag(data.id, data);
-                if (tag) {
-                    modalForm.resetFields();
-                    message.success('更新成功');
-                    setVisible(false);
-                    search.submit();
-                }
-            } else {
-                const tag = await tagStore.addTag(data);
-                if (tag) {
-                    modalForm.resetFields();
-                    message.success('添加成功');
-                    setVisible(false);
-                    search.submit();
-                }
-            }
-        } catch (error) {}
-    };
-    return (
-        <div>
-            <AdvancedSearchForm
-                form={form}
-                {...search}
-                extraActions={[
-                    {
-                        content: '添加',
-                        action: showModal,
-                    },
-                ]}
-            >
-                <Form.Item name="tagName" label="标签名">
-                    <Input placeholder="请输入标签名" />
-                </Form.Item>
-                <Form.Item name="displayName" label="展示名">
-                    <Input placeholder="请输入展示名" />
-                </Form.Item>
-            </AdvancedSearchForm>
-            <div className="mt-4">
-                <Table<TagDto>
-                    rowKey="id"
-                    {...{
-                        ...tableProps,
-                        pagination: {
-                            ...tableProps.pagination,
-                            showTotal: (total) => {
-                                return <div>总共：{total} 项</div>;
-                            },
-                            showSizeChanger: true,
-                        },
-                    }}
-                >
-                    <Table.Column<TagDto> title="Id" dataIndex="id" />
-                    <Table.Column<TagDto> title="标签名" dataIndex="tagName" />
-                    <Table.Column<TagDto>
-                        title="展示名"
-                        dataIndex="displayName"
-                    />
-                    <Table.Column<TagDto>
-                        title="操作"
-                        render={(recode) => {
-                            return (
-                                <div className="space-x-4">
-                                    <Button
-                                        type="primary"
-                                        onClick={() => getTag(recode)}
-                                    >
-                                        编辑
-                                    </Button>
-                                    <Button
-                                        type="primary"
-                                        danger
-                                        onClick={() => deleteTag(recode)}
-                                    >
-                                        删除
-                                    </Button>
-                                </div>
-                            );
-                        }}
-                    />
-                </Table>
-            </div>
-            <Modal
-                visible={visible}
-                title="添加一个新标签"
-                okText="确定"
-                cancelText="取消"
-                onCancel={() => {
-                    setVisible(false);
-                    modalForm.resetFields();
-                }}
-                onOk={() => {
-                    modalForm
-                        .validateFields()
-                        .then((values) => {
-                            addOrUpdateTag(values);
-                        })
-                        .catch((info) => {
-                            message.error('添加失败');
-                        });
-                }}
-            >
-                <Form
-                    form={modalForm}
-                    name="form_in_modal"
-                    labelCol={{ span: 6 }}
-                    wrapperCol={{ span: 18 }}
-                >
-                    <Form.Item name="id" label="id" hidden>
-                        <Input />
-                    </Form.Item>
-                    <Form.Item
-                        name="tagName"
-                        label="标签名"
-                        rules={[
-                            {
-                                required: true,
-                                message: '请输入标签名',
-                            },
-                        ]}
-                    >
-                        <Input />
-                    </Form.Item>
-                    <Form.Item
-                        name="displayName"
-                        label="展示名"
-                        rules={[
-                            {
-                                required: true,
-                                message: '请输入展示名',
-                            },
-                        ]}
-                    >
-                        <Input type="textarea" />
-                    </Form.Item>
-                </Form>
-            </Modal>
-        </div>
-    );
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const addOrUpdateTag = async (data: TagDto) => {
+    try {
+      if (data.id) {
+        const tag = await tagStore.updateTag(data.id, data);
+        if (tag) {
+          modalForm.resetFields();
+          message.success("更新成功");
+          setVisible(false);
+          search.submit();
+        }
+      } else {
+        const tag = await tagStore.addTag(data);
+        if (tag) {
+          modalForm.resetFields();
+          message.success("添加成功");
+          setVisible(false);
+          search.submit();
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div>
+      <AdvancedSearchForm
+        form={form}
+        {...search}
+        extraActions={[
+          {
+            content: "添加",
+            action: showModal,
+          },
+        ]}
+      >
+        <Form.Item name="tagName" label="标签名">
+          <Input placeholder="请输入标签名" />
+        </Form.Item>
+        <Form.Item name="displayName" label="展示名">
+          <Input placeholder="请输入展示名" />
+        </Form.Item>
+      </AdvancedSearchForm>
+      <div className="mt-4">
+        <Table<TagDto>
+          rowKey="id"
+          {...{
+            ...tableProps,
+            pagination: {
+              ...tableProps.pagination,
+              showTotal: (total) => {
+                return <div>总共：{total} 项</div>;
+              },
+              showSizeChanger: true,
+            },
+          }}
+        >
+          <Table.Column<TagDto> title="Id" dataIndex="id" />
+          <Table.Column<TagDto> title="标签名" dataIndex="tagName" />
+          <Table.Column<TagDto> title="展示名" dataIndex="displayName" />
+          <Table.Column<TagDto>
+            title="操作"
+            render={(recode) => {
+              return (
+                <div className="space-x-4">
+                  <Button type="primary" onClick={() => getTag(recode)}>
+                    编辑
+                  </Button>
+                  <Button
+                    type="primary"
+                    danger
+                    onClick={() => deleteTag(recode)}
+                  >
+                    删除
+                  </Button>
+                </div>
+              );
+            }}
+          />
+        </Table>
+      </div>
+      <Modal
+        visible={visible}
+        title="添加一个新标签"
+        okText="确定"
+        cancelText="取消"
+        onCancel={() => {
+          setVisible(false);
+          modalForm.resetFields();
+        }}
+        onOk={() => {
+          modalForm
+            .validateFields()
+            .then((values) => {
+              addOrUpdateTag(values);
+            })
+            .catch(() => {
+              message.error("添加失败");
+            });
+        }}
+      >
+        <Form
+          form={modalForm}
+          name="form_in_modal"
+          labelCol={{ span: 6 }}
+          wrapperCol={{ span: 18 }}
+        >
+          <Form.Item name="id" label="id" hidden>
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="tagName"
+            label="标签名"
+            rules={[
+              {
+                required: true,
+                message: "请输入标签名",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="displayName"
+            label="展示名"
+            rules={[
+              {
+                required: true,
+                message: "请输入展示名",
+              },
+            ]}
+          >
+            <Input type="textarea" />
+          </Form.Item>
+        </Form>
+      </Modal>
+    </div>
+  );
 }
 export default Tags;
