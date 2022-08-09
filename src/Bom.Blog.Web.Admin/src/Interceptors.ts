b6a9bc8f556f9fbe5001f440a7a6e03facd78fcd
bomky <bom_ky@outlook.com>
2022-08-09 14:12:08 +0800

    添加统一的500错误处理

--- src/Bom.Blog.Web.Admin/src/Interceptors.ts
+++ src/Bom.Blog.Web.Admin/src/Interceptors.ts
@@ -1,43 +1,47 @@
-import axios from 'axios';
-import { message } from 'antd';
+import axios from "axios";
+import { message } from "antd";
 
 const interceptors = () => {
-    // Add a request interceptor
-    axios.interceptors.request.use(
-        function (config) {
-            // Do something before request is sent
-            // config.headers['platform-type'] = 'SC';
-            config.xsrfCookieName = 'XSRF-TOKEN';
-            config.xsrfHeaderName = 'RequestVerificationToken';
-            return config;
-        },
-        function (error) {
-            // Do something with request error
-            return Promise.reject(error);
-        }
-    );
+  // Add a request interceptor
+  axios.interceptors.request.use(
+    function (config) {
+      // Do something before request is sent
+      // config.headers['platform-type'] = 'SC';
+      config.xsrfCookieName = "XSRF-TOKEN";
+      config.xsrfHeaderName = "RequestVerificationToken";
+      return config;
+    },
+    function (error) {
+      // Do something with request error
+      return Promise.reject(error);
+    }
+  );
 
-    axios.interceptors.response.use(
-        (response) => response,
-        (error) => {
-            if (axios.isCancel(error)) {
-                return Promise.reject(error);
-            }
-            // const code = get(error, 'response.data.code');
-            // const err = get(error, 'response.data.message');
-            // if (code === '401101') {
-            //     // 如果没有权限，则跳到登录页
-            //     message.error('登录已失效，请重新登录');
-            //     window.location.href = `/login`;
-            // } else if (code !== '200') {
-            //     message.error({
-            //         content: err || 'error',
-            //         duration: 2,
-            //     });
-            // }
-            return Promise.reject(error);
-        }
-    );
+  axios.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (axios.isCancel(error)) {
+        return Promise.reject(error);
+      }
+      console.log(error);
+      if (error.response.status === 500) {
+        message.error(error.response.data.error.message);
+      }
+      // const code = get(error, 'response.data.code');
+      // const err = get(error, 'response.data.message');
+      // if (code === '401101') {
+      //     // 如果没有权限，则跳到登录页
+      //     message.error('登录已失效，请重新登录');
+      //     window.location.href = `/login`;
+      // } else if (code !== '200') {
+      //     message.error({
+      //         content: err || 'error',
+      //         duration: 2,
+      //     });
+      // }
+      return Promise.reject(error);
+    }
+  );
 };
 
 export default interceptors;
