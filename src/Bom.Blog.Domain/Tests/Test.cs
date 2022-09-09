e11273ab004f224e02584839fe4666ed30d35023
bomky <bom_ky@outlook.com>
2022-09-09 09:16:47 +0800

    添加c#格式配置支持

---
+++ src/Bom.Blog.Domain/Tests/Test.cs
@@ -0,0 +1,31 @@
+﻿using JetBrains.Annotations;
+using System;
+using Volo.Abp;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace Bom.Blog.Tests
+{
+    public class Test : FullAuditedAggregateRoot<Guid>
+    {
+        public string Name { get; private set; }
+        public string Url { get; private set; }
+        private Test()
+        {
+
+        }
+        internal Test(Guid id, [NotNull] string name, [NotNull] string url) : base(id)
+        {
+            Set(name, url);
+        }
+        private void Set([NotNull] string name, [NotNull] string url)
+        {
+            Name = Check.NotNullOrWhiteSpace(name, nameof(name), FriendLinkConst.MaxNameLength);
+            Url = Check.NotNullOrWhiteSpace(url, nameof(url), FriendLinkConst.MaxUrlLength);
+        }
+        internal Test Change([NotNull] string name, [NotNull] string url)
+        {
+            Set(name, url);
+            return this;
+        }
+    }
+}
