c9ba58d08926a0b458fcf3dab12045c95da72ea9
bomky <bom_ky@outlook.com>
2022-09-22 11:17:44 +0800

    添加新注册的用户发送欢迎邮件

---
+++ src/Bom.Blog.Application/EventHandles/AccountHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Entities.Events;
+using Volo.Abp.Emailing;
+using Volo.Abp.Emailing.Templates;
+using Volo.Abp.EventBus;
+using Volo.Abp.Identity;
+using Volo.Abp.TextTemplating;
+
+namespace Bom.Blog.EventHandles
+{
+    public class AccountHandler : ILocalEventHandler<EntityCreatedEventData<IdentityUser>>, ITransientDependency
+    {
+        private readonly IEmailSender emailSender;
+        private readonly ITemplateRenderer templateRenderer;
+
+        public AccountHandler(IEmailSender emailSender, ITemplateRenderer templateRenderer)
+        {
+            this.emailSender = emailSender;
+            this.templateRenderer = templateRenderer;
+        }
+        public async Task HandleEventAsync(EntityCreatedEventData<IdentityUser> eventData)
+        {
+            var content = await templateRenderer.RenderAsync(StandardEmailTemplates.Message, new { message = $"Hello {eventData.Entity.UserName}" });
+            await emailSender.QueueAsync(eventData.Entity.Email, "Welcome", content);
+        }
+    }
+}
