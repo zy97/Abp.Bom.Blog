95f0e7427bbdbc00f13ce335ed244428cbef4b6a
bomky <bom_ky@outlook.com>
2022-10-27 10:57:55 +0800

    添加swc

--- src/Bom.Blog.Web.Admin/vite.config.ts
+++ src/Bom.Blog.Web.Admin/vite.config.ts
@@ -3,11 +3,14 @@ import react from '@vitejs/plugin-react';
 import { setDefaultResultOrder } from 'dns'
 // import WindiCSS from 'vite-plugin-windicss';
 import vitePluginImp from 'vite-plugin-imp';
+import swc from 'unplugin-swc'
 // https://vitejs.dev/config/
 setDefaultResultOrder('verbatim')
 export default defineConfig({
     plugins: [
         react(),
+        swc.vite(),
+        swc.rollup(),
         // 用于antd按需加载，但很慢,内置支持了多个库
         vitePluginImp(),
     ],
