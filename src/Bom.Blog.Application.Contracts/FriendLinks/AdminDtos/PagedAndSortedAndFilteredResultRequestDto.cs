61ce52da74a62a564bbca1bdff9e20e69a781957
bomky <bom_ky@outlook.com>
2022-10-31 17:15:05 +0800

    删除FriendLink相关代码

--- src/Bom.Blog.Application.Contracts/FriendLinks/AdminDtos/PagedAndSortedAndFilteredResultRequestDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using Volo.Abp.Application.Dtos;
-
-namespace Bom.Blog.FriendLinks.AdminDtos
-{
-    public class PagedAndSortedAndFilteredResultRequestDto : PagedAndSortedResultRequestDto
-    {
-        public string Name { get; set; }
-    }
-}
