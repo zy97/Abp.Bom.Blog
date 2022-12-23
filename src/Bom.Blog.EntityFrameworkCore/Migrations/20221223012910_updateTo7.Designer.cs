6ae40289b65a5cfe998d11973bcbe0b89e95abf5
bomky <bom_ky@outlook.com>
2022-12-23 17:28:04 +0800

    abp升级

--- src/Bom.Blog.EntityFrameworkCore/Migrations/20221223012910_updateTo7.Designer.cs
+++ src/Bom.Blog.EntityFrameworkCore/Migrations/20221223012910_updateTo7.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Bom.Blog.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -11,9 +12,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace Bom.Blog.Migrations
 {
     [DbContext(typeof(BlogDbContext))]
-    partial class BlogDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221223012910_updateTo7")]
+    partial class updateTo7
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
