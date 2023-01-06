1944a51262071319be8c62efb5396ed825965cf9
bomky <bom_ky@outlook.com>
2023-01-06 17:30:12 +0800

    vite升级到4.0

--- src/Bom.Blog.Web.Admin/vite.config.ts
+++ src/Bom.Blog.Web.Admin/vite.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import react from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react-swc";
 import { setDefaultResultOrder } from 'dns'
 // import WindiCSS from 'vite-plugin-windicss';
 import vitePluginImp from 'vite-plugin-imp';
-import swc from 'unplugin-swc'
 // https://vitejs.dev/config/
 setDefaultResultOrder('verbatim')
 export default defineConfig({
     plugins: [
         react(),
-        swc.vite(),
-        swc.rollup(),
         // 用于antd按需加载，但很慢,内置支持了多个库
         vitePluginImp(),
     ],
