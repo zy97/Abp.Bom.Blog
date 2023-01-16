b33f6c8cae707194f741a721e217a2b68dded179
bomky <bom_ky@outlook.com>
2023-01-16 09:43:30 +0800

    依赖更新

--- src/Bom.Blog.Web.Admin/vite.config.ts
+++ src/Bom.Blog.Web.Admin/vite.config.ts
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import react from "@vitejs/plugin-react-swc";
 import { setDefaultResultOrder } from 'dns'
 // import WindiCSS from 'vite-plugin-windicss';
-// import vitePluginImp from 'vite-plugin-imp';
 // https://vitejs.dev/config/
 setDefaultResultOrder('verbatim')
 export default defineConfig({
     plugins: [
         react(),
-        // 用于antd按需加载，但很慢,内置支持了多个库
-        // vitePluginImp(),
         // 用于解决App主键热更新异常
         {
             name: "singleHMR",
@@ -19,22 +15,10 @@ export default defineConfig({
                     m.importedModules = new Set();
                     m.importers = new Set();
                 });
-
                 return modules;
             },
         },
     ],
-    css: {
-        preprocessorOptions: {
-            less: {
-                javascriptEnabled: true,
-                modifyVars: {
-                    // '@primary-color': '#4377F0', //设置antd主题色
-                    '@primary-color': 'red', //设置antd主题色
-                },
-            },
-        },
-    },
     server: {
         proxy: {
             '/api': {
